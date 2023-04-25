8a5f8ec4ef52eeede9876b8b0391ad3ed86b97ba
Maylon-tech <minatokiroisen77@gmail.com>
2023-04-25 11:50:26 +0900

    creating file and routes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>Nemoto</h2>
+      </header>
+    </div>
+  )
+}
+
+export default App;
